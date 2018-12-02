4854a5267d4ac5c4d1f466923d87c9a754d97ce6
Alex Renda <alex@renda.org>
2018-12-01 23:31:24 -0500

    wild refactor of nn, should be ~100x faster and better handles directories

--- src/graph_neural_net/nn.py
+++ src/graph_neural_net/nn.py
@@ -60,8 +60,8 @@ def graphs_json_to_graph_tuple_and_labels(graphs, index_maps=None):
             nodes[nidx, ast_type_index_map[n['ast_type']]] = 1
 
     n_edges = np.array(list(len(g['edges']) for g in graphs))
-    senders = np.empty(sum(n_edges))
-    receivers = np.empty(sum(n_edges))
+    senders = np.empty(sum(n_edges), dtype=np.int64)
+    receivers = np.empty(sum(n_edges), dtype=np.int64)
     edges = np.zeros((sum(n_edges), len(edge_type_index_map)))
 
     i = 0
@@ -83,13 +83,13 @@ def graphs_json_to_graph_tuple_and_labels(graphs, index_maps=None):
         labels.append(g_labels)
 
     gtuple = gn.graphs.GraphsTuple(
-        tf.constant(nodes, dtype=tf.float64),
-        tf.constant(edges, dtype=tf.float64),
-        tf.constant(receivers, dtype=tf.int32),
-        tf.constant(senders, dtype=tf.int32),
-        tf.zeros([len(graphs), 0], dtype=np.float64),
-        tf.constant(n_nodes, dtype=tf.int32),
-        tf.constant(n_edges, dtype=tf.int32)
+        nodes,
+        edges,
+        receivers,
+        senders,
+        np.zeros([len(graphs), 0], dtype=np.float64),
+        n_nodes,
+        n_edges,
     )
 
     return gtuple, labels, index_maps
@@ -108,18 +108,39 @@ def weights_and_labels_arr(graph, labels):
 
     return weights_arr, labels_arr
 
-def train(train_graph, train_labels, test_graph, test_labels, num_labels,
-          nepoch=1000, batch_size=16, report_ep=10):
+def concat_np(graphs):
+    n_node = np.concatenate([g.n_node for g in graphs])
+    n_edge = np.concatenate([g.n_edge for g in graphs])
+    nodes = np.concatenate([g.nodes for g in graphs])
+    edges = np.concatenate([g.edges for g in graphs])
+    globals = np.concatenate([g.globals for g in graphs])
 
+    receivers = []
+    senders = []
+
+    goffset = 0
+    for graph in graphs:
+        receivers.append(graph.receivers + goffset)
+        senders.append(graph.senders + goffset)
+        goffset += graph.nodes.shape[0]
+
+    receivers = np.concatenate(receivers)
+    senders = np.concatenate(senders)
+
+    return gn.graphs.GraphsTuple(nodes, edges, receivers, senders, globals, n_node, n_edge)
+
+
+def train(train_graphs, train_labels, test_graphs, test_labels, num_labels,
+          nepoch=1000, batch_size=16, report_ep=10):
     NODE_LATENT_SIZE = 128
     NODE_HIDDEN_SIZE = 256
     EDGE_LATENT_SIZE = 128
     EDGE_HIDDEN_SIZE = 256
     N_ITER = 10
 
-    (n_nodes, node_features_len) = train_graph.nodes.shape
-    (n_edges, edge_features_len) = train_graph.edges.shape
-    n_subgraphs = train_graph.n_node.shape[0]
+    node_features_len = train_graphs[0].nodes.shape[1]
+    edge_features_len = train_graphs[0].edges.shape[1]
+    n_train_graphs = len(train_graphs)
 
     encoder_module = gn.modules.InteractionNetwork(
         edge_model_fn=lambda: snt.nets.MLP([edge_features_len, EDGE_LATENT_SIZE]),
@@ -135,7 +156,7 @@ def train(train_graph, train_labels, test_graph, test_labels, num_labels,
     )
 
     def placeheld_loss():
-        g = gn.utils_tf._placeholders_from_graphs_tuple(train_graph, True)
+        g = gn.utils_tf._placeholders_from_graphs_tuple(train_graphs[0], True)
         weights = tf.placeholder(tf.float32, shape=[g.nodes.shape[0]])
         labs = tf.placeholder(tf.int64, shape=[g.nodes.shape[0]])
 
@@ -161,13 +182,16 @@ def train(train_graph, train_labels, test_graph, test_labels, num_labels,
         try:
             sess.run(tf.global_variables_initializer())
 
-            test_graph = gn.graphs.GraphsTuple(*map(sess.run, test_graph))
+            test_graphs_and_labels = [
+                (
+                    concat_np(test_graphs[i:i+batch_size]),
+                    test_labels[i:i+batch_size],
+                )
+                for i in tqdm.trange(0, len(test_graphs), batch_size, desc='Batching test graphs')
+            ]
 
-            for epno in tqdm.trange(nepoch):
-                subgraphs_and_labels = [
-                    (gn.utils_tf.get_graph(train_graph, i), train_labels[i])
-                    for i in range(n_subgraphs)
-                ]
+            for epno in tqdm.trange(nepoch, desc='Epoch'):
+                subgraphs_and_labels = list(zip(train_graphs, train_labels))
                 random.shuffle(subgraphs_and_labels)
                 graphs, labels = zip(*subgraphs_and_labels)
 
@@ -176,22 +200,17 @@ def train(train_graph, train_labels, test_graph, test_labels, num_labels,
                 n_preds = 0
 
                 subgraphs = [
-                    gn.utils_tf.concat(graphs[i:i + batch_size], 0)
-                    for i in range(0, n_subgraphs, batch_size)
-                ]
-                subgraphs = [
-                    gn.graphs.GraphsTuple(*map(sess.run, subgraph))
-                    for subgraph in subgraphs
+                    concat_np(graphs[i:i + batch_size])
+                    for i in tqdm.trange(0, n_train_graphs, batch_size, desc='Batching train graphs')
                 ]
 
-                for i, subgraph in zip(tqdm.trange(0, n_subgraphs, batch_size), subgraphs):
+                for i, subgraph in zip(tqdm.trange(0, n_train_graphs, batch_size, desc='Batch'), subgraphs):
                     sublabels = labels[i:i+batch_size]
 
                     weights_arr, labels_arr = weights_and_labels_arr(subgraph, sublabels)
                     feed_dict = gn.utils_tf.get_feed_dict(plh_g, subgraph)
                     feed_dict[plh_w] = weights_arr
                     feed_dict[plh_l] = labels_arr
-
 
                     if epno % report_ep == 0:
                         m_loss, m_correct, _ = sess.run([loss, corr, train_fun], feed_dict)
@@ -210,26 +229,33 @@ def train(train_graph, train_labels, test_graph, test_labels, num_labels,
                         )
                     )
 
-                    weights_arr, labels_arr = weights_and_labels_arr(test_graph, test_labels)
-                    feed_dict = gn.utils_tf.get_feed_dict(plh_g, test_graph)
-                    feed_dict[plh_w] = weights_arr
-                    feed_dict[plh_l] = labels_arr
+                    test_sum_n_preds = 0
+                    test_sum_loss = 0
+                    test_sum_correct = 0
+                    for (m_test_graph, m_test_labels) in test_graphs_and_labels:
+                        weights_arr, labels_arr = weights_and_labels_arr(m_test_graph, m_test_labels)
+                        feed_dict = gn.utils_tf.get_feed_dict(plh_g, m_test_graph)
+                        feed_dict[plh_w] = weights_arr
+                        feed_dict[plh_l] = labels_arr
 
-                    m_loss, m_correct = sess.run([loss, corr], feed_dict)
-                    m_n_preds = int(weights_arr.sum())
+                        m_loss, m_correct = sess.run([loss, corr], feed_dict)
+
+                        test_sum_n_preds += int(weights_arr.sum())
+                        test_sum_loss += m_loss
+                        test_sum_correct += int(m_correct)
 
                     tqdm.tqdm.write(
                         'Test: mean loss: {:.2f}, Accuracy: {:.2f} ({}/{})'.format(
-                            m_loss / m_n_preds,
-                            m_correct / m_n_preds,
-                            int(m_correct), m_n_preds,
+                            test_sum_loss / test_sum_n_preds,
+                            test_sum_correct / test_sum_n_preds,
+                            test_sum_correct, test_sum_n_preds,
                         )
                     )
-
 
         except KeyboardInterrupt:
             print('Caught SIGINT!')
         finally:
+            print('Saving model...')
             dname = 'graph_{}'.format(time.strftime('%Y-%m-%d-%H:%M:%S'))
             dname = os.path.join(_DIRNAME, 'models', dname)
             os.makedirs(dname, exist_ok=True)
@@ -244,40 +270,86 @@ def train(train_graph, train_labels, test_graph, test_labels, num_labels,
 
 def main():
     parser = argparse.ArgumentParser(description='Train and test using the Deepmind GNN framework.')
-    parser.add_argument('graphs', nargs='+', help='A graph file to process')
+    parser.add_argument('--file', '-f', nargs='+', help='Individual graph files to process', default=[])
+    parser.add_argument('--dir', '--directory', '-d', nargs='+', help='Individual project directories to process', default=[])
+    parser.add_argument('--project', '-p', nargs='+', help='Directories containing a list of directories (equivalent to --dir PROJECT/*)', default=[])
     args = parser.parse_args()
 
-    graphs = []
-    for graph_f in args.graphs:
-        with open(graph_f) as f:
-            graphs.append(json.load(f))
+    def collect_directory(directory):
+        files = []
+        for (root, _, fnames) in os.walk(directory):
+            for fname in fnames:
+                if fname.endswith('.json'):
+                    files.append(os.path.join(root, fname))
+        return (directory, files)
 
-    graph_tuple, labels, index_maps = graphs_json_to_graph_tuple_and_labels(graphs)
+    # list of (dir name, [graph file]) tuples
+    project_graph_files = []
+
+    project_graph_files.extend(map(lambda x: (x, [x]), args.file))
+
+    for directory in args.dir:
+        project_graph_files.append(collect_directory(directory))
+
+    for project in args.project:
+        for directory in os.listdir(project):
+            project_graph_files.append(collect_directory(os.path.join(project, directory)))
+
+    project_graph_json = []
+    split_index_map = [0]
+    split_idx_cumsum = 0
+    for (_, files) in project_graph_files:
+        jsons = []
+        for fname in files:
+            with open(fname) as f:
+                jsons.append(json.load(f))
+        project_graph_json.append(jsons)
+        split_idx_cumsum += len(files)
+        split_index_map.append(split_idx_cumsum)
+
+    all_graphs = [graph for graphs in project_graph_json for graph in graphs]
+
+    graph_tuple, labels, index_maps = graphs_json_to_graph_tuple_and_labels(all_graphs)
     n_labels = len(index_maps.label_index_map)
 
+    def graphs_labs_of_underlying_idxs(all_idxs):
+        graphs = [gn.utils_np.get_graph(graph_tuple, i) for i in all_idxs]
+        labs = [labels[i] for i in all_idxs]
+        return concat_np(graphs), labs
+
+    dir_graphs_labs = []
+    for idx in range(len(split_index_map) - 1):
+        dir_graphs_labs.append(graphs_labs_of_underlying_idxs(list(range(split_index_map[idx], split_index_map[idx + 1]))))
+
     split_perc = 0.8
-    idxs = list(range(len(labels)))
+    idxs = list(range(len(dir_graphs_labs)))
     random.shuffle(idxs)
     n_train = math.floor(split_perc * len(idxs))
 
-    def graphs_labs_of_idxs(all_idxs):
-        graphs = [gn.utils_tf.get_graph(graph_tuple, i) for i in all_idxs]
-        labs = [labels[i] for i in all_idxs]
-        return gn.utils_tf.concat(graphs, 0), labs
+    def graphs_labs_of_project_idxs(idxs):
+        graphs = []
+        labels = []
+        for idx in idxs:
+            graph, labs = dir_graphs_labs[idx]
+            for j in range(len(labs)):
+                graphs.append(gn.utils_np.get_graph(graph, j))
+                labels.append(labs[j])
 
-    train_graphs, train_labels = graphs_labs_of_idxs(idxs[:n_train])
-    test_graphs, test_labels = graphs_labs_of_idxs(idxs[n_train:])
+        return graphs, labels
+
+    train_graphs, train_labels = graphs_labs_of_project_idxs(idxs[:n_train])
+    test_graphs, test_labels = graphs_labs_of_project_idxs(idxs[n_train:])
 
     print('train set ({} nodes, {} labels): =====\n{}\n====='.format(
-        train_graphs.nodes.shape[0],
+        sum(sum(g.n_node) for g in train_graphs),
         sum(map(len, train_labels)),
-        '\n'.join(args.graphs[i] for i in idxs[:n_train])
+        '\n'.join(project_graph_files[i][0] for i in idxs[:n_train])
     ))
 
     print('test set ({} nodes, {} labels): =====\n{}\n====='.format(
-        test_graphs.nodes.shape[0],
+        sum(sum(g.n_node) for g in train_graphs),
         sum(map(len, test_labels)),
-        '\n'.join(args.graphs[i] for i in idxs[n_train:])
+        '\n'.join(project_graph_files[i][0] for i in idxs[n_train:])
     ))
 
     train(train_graphs, train_labels, test_graphs, test_labels, n_labels)
