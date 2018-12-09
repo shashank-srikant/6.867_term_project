8b52a2a91d839bec2fe26738c43f83a976d7a9a9
shashank <shash@mit.edu>
2018-12-09 22:16:46 +0000

    moved method reverse_edge to base class Edge

--- src/ast2graph/edge_use_def.ts
+++ src/ast2graph/edge_use_def.ts
@@ -129,23 +129,6 @@ export class EdgeUseDef extends Edge {
         }
     }
 
-    private reverse_edge(edge_list:GraphEdge[], node_map:Map<ts.Node, number>): GraphEdgeDebug[]{
-        function node_name(ed: number): string{
-            let st = ts.SyntaxKind[get_by_value(node_map, ed).kind];
-            return st;
-        }
-        var rev_edge_list: GraphEdgeDebug[] = [];
-        for(let i = 0; i<edge_list.length; i++) {
-            let rev_edge:GraphEdgeDebug = {
-                'src': node_name(edge_list[i]['src']),
-                'dst': node_name(edge_list[i]['dst']),
-                'edge_type': edge_list[i]['edge_type']
-            }
-            rev_edge_list.push(rev_edge);
-        }
-        return rev_edge_list;
-    }
-
     public visit_tree(node: ts.Node, edges: GraphEdge[],
                         parent: number, checker: ts.TypeChecker,
                         node_map: Map<ts.Node, number>):GraphEdge[]{
